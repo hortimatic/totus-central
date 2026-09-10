4b3914a8a8e0d3f3e83feecdcc2070a05fda8db6
hortimatic <hortimatic@gmail.com>
2026-09-10 16:11:14 +0200

    Conectar runtime limpio con Supabase nuevo

---
+++ runtime-config.js
@@ -0,0 +1,5 @@
+window.TOTUS_CONFIG = Object.freeze({
+  supabaseUrl: 'https://qgyufbjytcewuevbketz.supabase.co',
+  supabaseKey: 'sb_publishable_-aBzFQe7z9uTfphD-qZyxg_GqDuI5qQ',
+  timezone: 'Europe/Madrid'
+});
